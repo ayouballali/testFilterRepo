5940ce56eb357612818185d3adba82c12438bec8
aallali <ayoub.allali@oracle.com>
2024-08-30 13:59:19 +0100

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation (files("lib/slf4j-api-2.0.16.jar"))
+//    implementation (files("lib/slf4j-api-2.0.16.jar"))
+    implementation("org.slf4j:slf4j-simple:2.0.15")
 
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class);
 
+        Logger logger = LoggerFactory.getLogger(Main.class);
         System.out.println("Hello world!");
         logger.info("Hello world !");
         // a comment
